194d0a6ff560276caa36a14c60cabaaaff0adf33
Gregson Murcia Castro <gregoryinnovo@gmail.com>
2022-01-04 14:49:23 -0500

    config fonts loader

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
+    assetModuleFilename: "assets/images/[hash][ext][query]",
   },
   resolve: {
     extensions: [".js"],
@@ -30,6 +31,24 @@ module.exports = {
       {
         test: /\.png/,
         type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 10000,
+            mimeType: "application/font-woff",
+            name: "[name].[ext]",
+            outputPath: "./assets/fonts/",
+            publicPath: "./assets/fonts/",
+            esModule: false,
+          },
+        },
+        generator: {
+          filename: "assets/fonts/[hash][ext]",
+        },
       },
     ],
   },
