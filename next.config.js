8180882e6ea85578ef58adaf753afeb762edb92e
Gregson Murcia Castro <gregoryinnovo@gmail.com>
2022-01-19 19:00:11 -0500

    config next project

--- next.config.js
+++ next.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   reactStrictMode: true,
-}
+  // we can environments values to production
+  env: {
+    customKey: 'customValue',
+  },
+  // location of the project
+  basePath: '/dist',
+  compress: true,
+  async redirects() {
+    return [
+      {
+        source: '/hey',
+        destination: '/hello',
+        permanent: true,
+      },
+    ];
+  },
+};
