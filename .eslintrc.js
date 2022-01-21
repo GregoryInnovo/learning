22e0166416ae37b54a2499bade3f27263495e123
Gregson Murcia Castro <gregoryinnovo@gmail.com>
2022-01-20 22:11:55 -0500

    [ADD] ga and fix _document config

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,5 +9,6 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:jsx-a11y/recommended', 'plugin:prettier/recommended', 'next/core-web-vitals'],
   rules: {
     semi: ['error', 'always'],
+    'prettier/prettier': 0, // with 1 is a warning, and with 2 is required
   },
 };
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,22 @@
 import Header from '@components/Header';
+import Script from 'next/script';
 import AppContext from '@context/AppContext';
 import useInitialState from '@hooks/useInitialState';
-import '../styles/globals.css';
+import '@styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   const initialState = useInitialState();
 
   return (
     <AppContext.Provider value={initialState}>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-2Q1766XQNX" />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-2Q1766XQNX');`}
+      </Script>
       <Header />
       <Component {...pageProps} />
     </AppContext.Provider>
