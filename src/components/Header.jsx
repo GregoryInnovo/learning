94b00c7b640e8b10ccc8d83751fb9deba0f0e0fc
Gregson Murcia Castro <gregoryinnovo@gmail.com>
2022-01-20 22:36:10 -0500

    [FIX] next warnings and errors

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,65 +1,61 @@
-import React, { useState, useContext } from 'react';
+import React, { useContext } from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 import Menu from '@components/Menu';
 import menu from '@icons/icon_menu.svg';
-import shopping_card from '@icons/icon_shopping_cart.svg';
+import shoppingCart from '@icons/icon_shopping_cart.svg';
 import logo from '@logos/logo_yard_sale.svg';
 import AppContext from '@context/AppContext';
 import MyOrder from '@containers/MyOrder';
 import styles from '@styles/Header.module.scss';
-import Link from 'next/link';
 
 const Header = () => {
-  const [toggle, setToggle] = useState(false);
-  const [toggleOrders, setToggleOrders] = useState(false);
-  const { state } = useContext(AppContext);
-
-  const handleToggle = () => {
-    setToggle(!toggle);
-  };
+  const { state, toggleOrder, toggleMenu } = useContext(AppContext);
 
   return (
-    <nav className={styles.Nav}>
-      <Image src={menu} alt="menu" className={styles.menu} />
-      <div className={styles['navbar-left']}>
-        <Link href="/">
-          <Image src={logo} alt="logo" className={styles['nav-logo']} />
-        </Link>
-        <ul>
-          <li>
-            <Link href="/">All</Link>
-          </li>
-          <li>
-            <Link href="/">Clothes</Link>
-          </li>
-          <li>
-            <Link href="/">Electronics</Link>
-          </li>
-          <li>
-            <Link href="/">Furnitures</Link>
-          </li>
-          <li>
-            <Link href="/">Toys</Link>
-          </li>
-          <li>
-            <Link href="/">Others</Link>
-          </li>
-        </ul>
-      </div>
-      <div className={styles['navbar-right']}>
-        <ul>
-          <li className={styles['navbar-email']} onClick={handleToggle}>
-            foodhy@example.com
-          </li>
-          <li className={styles['navbar-shopping-cart']} onClick={() => setToggleOrders(!toggleOrders)}>
-            <Image src={shopping_card} alt="shopping cart" />
-            {state.cart.length > 0 ? <div>{state.cart.length}</div> : null}
-          </li>
-        </ul>
-      </div>
-      {toggle && <Menu />}
-      {toggleOrders && <MyOrder />}
-    </nav>
+    <>
+      <nav className={styles.Nav}>
+        <Image src={menu.src} alt="menu" className={styles.menu} />
+        <div className={styles['navbar-left']}>
+          <Link href="/" passHref>
+            <Image src={logo} alt="logo" className={styles['nav-logo']} />
+          </Link>
+          <ul>
+            <li>
+              <Link href="/">Linkll</Link>
+            </li>
+            <li>
+              <Link href="/">Clothes</Link>
+            </li>
+            <li>
+              <Link href="/">Electronics</Link>
+            </li>
+            <li>
+              <Link href="/">Furnitures</Link>
+            </li>
+            <li>
+              <Link href="/">Toys</Link>
+            </li>
+            <li>
+              <Link href="/">Others</Link>
+            </li>
+          </ul>
+        </div>
+        <div className={styles['navbar-right']}>
+          <ul>
+            <li className={(styles['more-clickable-area'], styles['navbar-email'], styles.pointer)} onClick={() => toggleMenu()} aria-hidden="true">
+              foodhyshop@example.com
+            </li>
+            <li className={styles['navbar-shopping-cart']} onClick={() => toggleOrder()} aria-hidden="true">
+              <Image className={(styles['more-clickable-area'], styles.pointer)} src={shoppingCart} alt="shopping cart" />
+              {state.cart.length > 0 ? <div>{state.cart.length}</div> : null}
+            </li>
+          </ul>
+        </div>
+        {state.menuIsOpen && <Menu />}
+      </nav>
+      {state.orderIsOpen && <MyOrder />}
+    </>
   );
 };
 
--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,28 +1,32 @@
 import React, { useContext } from 'react';
-import addToCardIcon from '@icons/bt_add_to_cart.svg';
-import AppContext from '@context/AppContext';
 import Image from 'next/image';
+import AppContext from '@context/AppContext';
+import addToCartImage from '@icons/bt_add_to_cart.svg';
+import addedToCartImage from '@icons/bt_added_to_cart.svg';
 import styles from '@styles/ProductItem.module.scss';
 
 const ProductItem = ({ product }) => {
-  const { images, title, price } = product;
-
-  const { addToCart } = useContext(AppContext);
+  const { state, addToCart } = useContext(AppContext);
 
   const handleClick = (item) => {
+    console.log('in cart: ', state.cart.includes(item));
     addToCart(item);
   };
 
   return (
     <div className={styles.ProductItem}>
-      <Image src={images[0]} width={240} height={240} alt={title} />
+      <Image src={product.images[0]} width={240} height={240} alt={product.title} />
       <div className={styles['product-info']}>
         <div>
-          <p>${price}</p>
-          <p>{title}</p>
+          <p>${product.price}</p>
+          <p>{product.title}</p>
         </div>
-        <figure onClick={() => handleClick(product)}>
-          <Image src={addToCardIcon} width={50} height={50} alt="icon to add" />
+        <figure className={styles['more-clickable-area']} onClick={() => handleClick(product)} aria-hidden="true">
+          {state.cart.includes(product) ? (
+            <Image className={(styles.disabled, styles['add-to-cart-btn'])} src={addedToCartImage} alt="added to cart" />
+          ) : (
+            <Image className={(styles['add-to-cart-btn'], styles.pointer)} src={addToCartImage} alt="add to cart" />
+          )}
         </figure>
       </div>
     </div>
