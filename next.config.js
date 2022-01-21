5f16b2b5ceee1f7ffb97b953a2d071a2546fa61d
Gregson Murcia Castro <gregoryinnovo@gmail.com>
2022-01-21 00:05:44 -0500

    [ADD]  support to PWA

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
-module.exports = {
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    register: true,
+    mode: 'production',
+    disable: false,
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest\.json$/],
+  },
   reactStrictMode: true,
   images: {
     domains: ['placeimg.com'],
@@ -19,4 +30,4 @@ module.exports = {
       },
     ];
   }, */
-};
+});
