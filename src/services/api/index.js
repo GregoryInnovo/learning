0928a302d232cb127eb4dd47fd8fa67543848387
Gregson Murcia Castro <gregoryinnovo@gmail.com>
2022-05-01 01:18:49 -0500

    [ADD] Dynamic routes

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -12,8 +12,8 @@ const endpoints = {
     allProducts: `${API}/api/${VERSION}/products`,
     getProducts: (limit, offset) => `${API}/api/${VERSION}/products?limit=${limit}&offset=${offset}`,
     addProducts: `${API}/api/${VERSION}/products`,
-    updateProducts: (id) => `${API}/api/${VERSION}/products/${id}/`,
-    deleteProducts: (id) => `${API}/api/${VERSION}/products/${id}/`,
+    updateProduct: (id) => `${API}/api/${VERSION}/products/${id}/`,
+    deleteProduct: (id) => `${API}/api/${VERSION}/products/${id}/`,
   },
   users: {
     getAllUsers: `${API}/api/${VERSION}/users`,
